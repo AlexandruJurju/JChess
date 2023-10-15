6ad54768cf718edfb684894c1274a9a83dff1731
Alexandru Jurju <alexandru.jurju@ulbisbiu.ro>
2023-10-15 13:22:54 +0300

    knight moves

--- ChessText/ChessLogic/Pieces/Knight.cs
+++ ChessText/ChessLogic/Pieces/Knight.cs
@@ -1,25 +1,44 @@
 ﻿using System.Collections.Generic;
 
-namespace ChessLogic
-{
-	public class Knight : Piece
-	{
+namespace ChessLogic {
+	public class Knight : Piece {
 		public override PieceType Type => PieceType.Pawn;
 
 		public override Player Color { get; }
 
-		public Knight(Player color)
-		{
+		public Knight(Player color) {
 			Color = color;
 		}
 
-		public override List<Move> GenerateMoves(Position origin, BoardModel board)
-		{
-			throw new System.NotImplementedException();
+		public override List<Move> GenerateMoves(Position origin, BoardModel board) {
+			List<Move> moves = new List<Move>();
+			List<Position> finalPositions = GetAllPossibleDestinations(origin, board);
+			foreach (Position finalPosition in finalPositions) {
+				moves.Add(new Move(origin, finalPosition));
+			}
+			return moves;
 		}
 
 		public override List<Position> GetAllPossibleDestinations(Position origin, BoardModel board) {
-			throw new System.NotImplementedException();
+			List<Position> positions = new List<Position>();
+
+			Position NEEdge = origin + Direction.NE * 2;
+			positions.Add(NEEdge + Direction.S);
+			positions.Add(NEEdge + Direction.W);
+
+			Position NWEdge = origin + Direction.NW * 2;
+			positions.Add(NEEdge + Direction.E);
+			positions.Add(NEEdge + Direction.S);
+
+			Position SWEdge = origin + Direction.SW * 2;
+			positions.Add(NEEdge + Direction.N);
+			positions.Add(NEEdge + Direction.E);
+
+			Position SEEdge = origin + Direction.SE * 2;
+			positions.Add(NEEdge + Direction.E);
+			positions.Add(NEEdge + Direction.N);
+
+			return positions;
 		}
 	}
 }
