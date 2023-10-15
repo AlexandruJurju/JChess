90941a310e3373a776bb593e8aa68cb54139181a
Alexandru Jurju <alexandru.jurju@ulbisbiu.ro>
2023-10-15 13:10:39 +0300

    rook, bishop, queen moves

--- ChessText/ChessLogic/Pieces/Rook.cs
+++ ChessText/ChessLogic/Pieces/Rook.cs
@@ -10,12 +10,34 @@ namespace ChessLogic {
 			Color = color;
 		}
 
+		private static readonly Direction[] moveDirections = new Direction[]{
+			Direction.N, Direction.S, Direction.E, Direction.W
+		};
+
 		public override List<Move> GenerateMoves(Position origin, BoardModel board) {
-			throw new System.NotImplementedException();
+			List<Move> moves = new List<Move>();
+			List<Position> finalPositions = GetAllPossibleDestinations(origin, board);
+			foreach (Position finalPosition in finalPositions) {
+				moves.Add(new Move(origin, finalPosition));
+			}
+			return moves;
 		}
 
 		public override List<Position> GetAllPossibleDestinations(Position origin, BoardModel board) {
-			throw new System.NotImplementedException();
+			List<Position> result = new List<Position>();
+
+			foreach (Direction dir in moveDirections) {
+				for (Position pos = origin + dir; board.IsInsideBoard(pos); pos += dir) {
+					if (board.IsEmptyPosition(pos)) {
+						result.Add(pos);
+					} else if (board[pos].Color != this.Color) {
+						result.Add(pos);
+						continue;
+					}
+				}
+			}
+
+			return result;
 		}
 	}
 }
