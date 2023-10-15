239190ef4611c7d732c2a76a2042a2590db5960a
Alex <alexandru.i.jurju@gmail.com>
2023-10-15 19:01:12 +0300

    starting board cells

--- FormsChess/MainForm.cs
+++ FormsChess/MainForm.cs
@@ -10,10 +10,23 @@ namespace FormsChess {
 			this.Location = new Point(0, 0);
 			this.CenterToScreen();
 
-			Display display = new Display();
-			this.Controls.Add(display.PlayerIndicator);
-			this.Controls.Add(display.DepthComboBox);
-			this.Controls.Add(display.DepthLabel);
+			Display display = new Display(this);
+
+
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					PictureBox pictureBox = new PictureBox();
+					pictureBox.Size = new Size(60, 60);
+					pictureBox.Location = new Point(i * 60 + 75, j * 60 + 75);
+					if (i % 2 == j % 2) {
+						pictureBox.BackColor = Color.FromArgb(255, 102, 68, 58);
+					} else {
+						pictureBox.BackColor = Color.FromArgb(255, 245, 230, 191);
+					}
+
+					this.Controls.Add(pictureBox);
+				}
+			}
 
 
 		}
--- FormsChess/View/Display.cs
+++ FormsChess/View/Display.cs
@@ -14,7 +14,11 @@ namespace View {
 
 		public Cell[,] ViewCells = new Cell[8, 8];
 
-		public Display() {
+		public Form Form { get; set; }
+
+		public Display(Form form) {
+			Form = form;
+
 			DepthComboBox = new ComboBox();
 			DepthComboBox.Location = new Point(950, 300);
 			DepthComboBox.Size = new Size(100, 100);
@@ -25,12 +29,14 @@ namespace View {
 			DepthComboBox.Items.Add("4");
 			DepthComboBox.SelectedItem = "Random";
 			DepthComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+			Form.Controls.Add(DepthComboBox);
 
 			DepthLabel = new Label();
 			DepthLabel.Text = "Max depth";
 			DepthLabel.Size = new Size(150, 150);
 			DepthLabel.Location = new Point(950, 260);
 			DepthLabel.Font = new Font("Roboto", 12, FontStyle.Regular);
+			Form.Controls.Add(DepthLabel);
 
 			PlayerIndicator = new Label();
 			PlayerIndicator.Visible = true;
@@ -41,6 +47,8 @@ namespace View {
 			PlayerIndicator.BackColor = Color.White;
 			PlayerIndicator.Font = new Font("Times New Roman", 20);
 			PlayerIndicator.TextAlign = ContentAlignment.MiddleCenter;
+			Form.Controls.Add(PlayerIndicator);
+
 		}
 
 		public void UpdateIndicatorColor(Player player) {
@@ -50,25 +58,6 @@ namespace View {
 			} else {
 				PlayerIndicator.ForeColor = Color.Black;
 				PlayerIndicator.Text = "P2";
-			}
-		}
-
-		public void InitCells() {
-			for (int i = 0; i < 8; i++) {
-				for (int j = 0; j < 8; j++) {
-					Cell cell = new Cell(i, j);
-					cell.Size = new Size(60, 60);
-					cell.Location = new Point(i * 60 + 75, j * 60 + 75);
-					cell.Click += new EventHandler(form.clickOnCell);
-					if (i % 2 == j % 2) {
-						cell.BackColor = Color.FromArgb(255, 102, 68, 58);
-					} else {
-						cell.BackColor = Color.FromArgb(255, 245, 230, 191);
-					}
-					cell.Color = cell.BackColor;
-
-					ViewCells[j, i] = cell;
-				}
 			}
 		}
 
